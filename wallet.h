cc997fd4668ff82fd647b6c9b65ce559b5f92e08
Kenny Luong <kenny.k.luong@gmail.com>
2016-04-13 17:04:07 -0700

    Update wallet.h

--- wallet.h
+++ wallet.h
@@ -7,14 +7,46 @@
 template <class T>
 class Wallet {
 private:
-  int walletSize;
-  bool isEmpty;
+    std::vector<Dollar> wallet;
 public:
-  Wallet();
-  ~Wallet();
-  void addCurrency(const Currency&);
-  void isExists();
-  void emptyWallet();
-}
+    Wallet() {
+        std::vector<Dollar> wallet;
+    }
+    ~Wallet() {
+        std::cout << "I am clearing the wallet" << std::endl;
+        wallet.clear();
+        std::cout << walletSize() << std::endl;
+    }
+
+    void addCurrency(const Dollar& d) {
+        int position = isExists(d.getName());
+        if (position == -1) {
+          wallet.push_back(d);
+        } else {
+          wallet.at(position) = wallet.at(position) + d;
+        }
+    }
+
+    int walletSize() {
+        return wallet.size();
+    }
+
+    int printContent(int index) {
+        return wallet.at(index).getWhole();
+    }
+
+    int isExists(std::string theName) {
+        for (int i = 0; i < walletSize(); i++) {
+          if (theName == wallet.at(i).getName()) {
+            return i;
+          }
+        }
+        return -1;
+    }
+
+    void clearWallet() {
+        wallet.clear();
+    }
+};
 
 #endif
